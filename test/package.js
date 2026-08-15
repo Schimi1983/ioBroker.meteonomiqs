6477e6d4d42a3d91a96a42c042be0c7b2236a030
Schimi <szymon.haiduk@world-mg.de>
2026-08-15 13:34:27 +0200

    Delete test directory

--- test/package.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const path = require('path');
-const { tests } = require('@iobroker/testing');
-
-// Validates package.json and io-package.json against the ioBroker requirements.
-tests.packageFiles(path.join(__dirname, '..'));
